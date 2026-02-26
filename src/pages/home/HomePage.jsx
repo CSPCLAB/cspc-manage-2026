c19689b7c41ae7ba102e89ee3d30e19b2810de64
dasol0312@gmail.com <dasol0312@gmail.com>
2026-02-26 17:38:00 +0900

    resolve merge conflicts

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,33 +1,21 @@
-<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
-=======
->>>>>>> origin/main
 import styles from "./HomePage.module.css";
 import TopBar from "../../components/layout/TopBar";
 import WeekSchedulePanel from "./components/WeekSchedulePanel";
 import NoticePanel from "./components/NoticePanel";
 import MeetingPanel from "./components/MeetingPanel";
 import RequestsPanel from "./components/RequestsPanel";
-<<<<<<< HEAD
 import AdminAuthPanel from "./components/AdminAuthPanel"; // ✅ 추가
 import LateRankPanel from "./components/LateRankPanel"; // ✅ 추가
 
 export default function HomePage() {
   const NOTION_URL = import.meta.env.VITE_NOTION_URL || "https://www.notion.so/";
   const navigate = useNavigate();
-=======
-import LateRankPanel from "./components/LateRankPanel"; // ✅ 추가
-import { Link } from "react-router-dom";
-
-export default function HomePage() {
-  const NOTION_URL = import.meta.env.VITE_NOTION_URL || "https://www.notion.so/";
->>>>>>> origin/main
 
   return (
     <div className={styles.page}>
       <TopBar
         right={
-<<<<<<< HEAD
           <div className={styles.topRightButtons}>
             <button
               className={styles.notionBtn}
@@ -48,21 +36,6 @@ export default function HomePage() {
               노션 바로가기
             </button>
           </div>
-=======
-          <>
-            <Link className={styles.notionBtn} to="/d104">
-              실습실 관리
-            </Link>
-            <a
-              className={styles.notionBtn}
-              href={NOTION_URL}
-              target="_blank"
-              rel="noreferrer"
-            >
-              노션 바로가기
-            </a>
-          </>
->>>>>>> origin/main
         }
       />
 
@@ -70,10 +43,7 @@ export default function HomePage() {
         <div className={styles.left}>
           <div className={styles.leftTop}>
             <NoticePanel />
-<<<<<<< HEAD
             <AdminAuthPanel />
-=======
->>>>>>> origin/main
           </div>
           <div className={styles.leftBottom}>
             <WeekSchedulePanel />
