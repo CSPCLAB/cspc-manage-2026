c41da0c051fd757829603e551aaaa3d4a0238d4d
dasol0312@gmail.com <dasol0312@gmail.com>
2026-03-07 04:06:14 +0900

    fix: 공지사항 수정(최종 아님)

--- src/pages/home/components/NoticePanel.jsx
+++ src/pages/home/components/NoticePanel.jsx
@@ -1,18 +1,35 @@
 import Panel from "../../../components/layout/Panel";
 import styles from "./NoticePanel.module.css";
 
-const DUMMY = Array.from({ length: 1 }, (_, i) => ({
-  id: i + 1,
-  title: `CSPC 랩실 사용수칙`,
-  category: i % 2 === 0 ? "랩실 사용수칙" : "관리 수칙",
-}));
-
 export default function NoticePanel() {
   return (
     <Panel title="공지사항" className={styles.panelFull}>
-      <ul className={styles.list}>
-        <h3>vercel 확인용 배포</h3>
-      </ul>
+      <div className={styles.list}>
+        <h4>1. CSPC LAB 관리수칙</h4>
+        <ul>
+          <li>관리 시간에 맞춰 랩실에 오기</li>
+          <li>첫 타임 관리자는 랩실 문 열기</li>
+          <li>마지막 타임 관리자는 관리 종료 후 랩실 잠그기</li>
+          <li>교수님 또는 조교님의 요청사항을 처리하고, 혼자 해결하기 어려울 경우 단톡방에 문의하기</li>
+        </ul>
+        <h4>2. CSPC LAB 관리실 이용수칙</h4>
+        <ul>
+          <li>외부인 출입 자제</li>
+          <li>음주, 숙박 금지</li>
+          <li>본인이 이용한 자리는 깨끗하게 치우기</li>
+          <li>수업이 있는 시간에는 관리자실에서 음식 섭취 금지</li>
+          <li>쓰레기통이 꽉차있다면 비우기</li>
+        </ul>
+        <h4>3. CSPC LAB 내부 행사</h4>
+        <ul>
+          <li>신입부원 모집</li>
+          <li>신입생 환영회</li>
+          <li>봄소풍</li>
+          <li>여름 MT</li>
+          <li>크리스마스 파티</li>
+          <li>겨울 MT</li>
+        </ul>
+      </div>
     </Panel>
   );
 }
